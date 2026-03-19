12f55d9cc07ccaf3baa5ebadbcbc613a74981421
Hidegg <hideg.wok@gmail.com>
2026-03-20 01:11:48 +0200

    Add missing migration for notite column on servicii

---
+++ migrations/versions/b2c3d4e5f6a7_add_notite_to_servicii.py
@@ -0,0 +1,26 @@
+"""add notite to servicii
+
+Revision ID: b2c3d4e5f6a7
+Revises: 91bab3cfb574
+Create Date: 2026-03-20 00:00:00.000000
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'b2c3d4e5f6a7'
+down_revision = 'a1b2c3d4e5f6'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    with op.batch_alter_table('servicii', schema=None) as batch_op:
+        batch_op.add_column(sa.Column('notite', sa.String(length=500), nullable=True))
+
+
+def downgrade():
+    with op.batch_alter_table('servicii', schema=None) as batch_op:
+        batch_op.drop_column('notite')
